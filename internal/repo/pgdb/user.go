7363fd89236e87f2b88aab49227e7df3625d0e19
dannromm <danilka.2002.lol@gmail.com>
2022-10-17 15:37:09 +0300

    added GetUserBySmth functions to user repo

--- internal/repo/pgdb/user.go
+++ internal/repo/pgdb/user.go
@@ -51,7 +51,7 @@ func (r *UserRepo) CreateUser(ctx context.Context, user entity.User) (int, error
 
 func (r *UserRepo) GetUserByUsernameAndPassword(ctx context.Context, username, password string) (entity.User, error) {
 	sql, args, err := r.Builder.
-		Select("id", "name", "username", "password", "email").
+		Select("*").
 		From("users").
 		Where("username = ? AND password = ?", username, password).
 		ToSql()
@@ -62,13 +62,108 @@ func (r *UserRepo) GetUserByUsernameAndPassword(ctx context.Context, username, p
 	}
 
 	var user entity.User
-	err = r.Pool.QueryRow(ctx, sql, args...).Scan(&user.Id, &user.Name, &user.Username, &user.Password, &user.Email)
+	err = r.Pool.QueryRow(ctx, sql, args...).Scan(
+		&user.Id,
+		&user.Name,
+		&user.Username,
+		&user.Password,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+		&user.Description,
+		&user.ArticlesCount,
+		&user.CommentsCount,
+		&user.FavoritesArticlesCount,
+		&user.FavoritesCommentsCount,
+		&user.FollowersCount,
+		&user.FollowingCount,
+	)
 	if err != nil {
 		log.Errorf("UserRepo.GetUserByUsernameAndPassword - r.Pool.QueryRow: %v", err)
 		if err == pgx.ErrNoRows {
 			return entity.User{}, repoerrs.ErrUserNotFound
 		}
 		return entity.User{}, fmt.Errorf("UserRepo.GetUserByUsernameAndPassword - r.Pool.QueryRow: %v", err)
+	}
+
+	return user, nil
+}
+
+func (r *UserRepo) GetUserById(ctx context.Context, id int) (entity.User, error) {
+	sql, args, err := r.Builder.
+		Select("*").
+		From("users").
+		Where("id = ?", id).
+		ToSql()
+
+	if err != nil {
+		log.Errorf("UserRepo.GetUserById - r.Builder: %v", err)
+		return entity.User{}, fmt.Errorf("UserRepo.GetUserById - r.Builder: %v", err)
+	}
+
+	var user entity.User
+	err = r.Pool.QueryRow(ctx, sql, args...).Scan(
+		&user.Id,
+		&user.Name,
+		&user.Username,
+		&user.Password,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+		&user.Description,
+		&user.ArticlesCount,
+		&user.CommentsCount,
+		&user.FavoritesArticlesCount,
+		&user.FavoritesCommentsCount,
+		&user.FollowersCount,
+		&user.FollowingCount,
+	)
+	if err != nil {
+		log.Errorf("UserRepo.GetUserById - r.Pool.QueryRow: %v", err)
+		if err == pgx.ErrNoRows {
+			return entity.User{}, repoerrs.ErrUserNotFound
+		}
+		return entity.User{}, fmt.Errorf("UserRepo.GetUserById - r.Pool.QueryRow: %v", err)
+	}
+
+	return user, nil
+}
+
+func (r *UserRepo) GetUserByUsername(ctx context.Context, username string) (entity.User, error) {
+	sql, args, err := r.Builder.
+		Select("*").
+		From("users").
+		Where("username = ?", username).
+		ToSql()
+
+	if err != nil {
+		log.Errorf("UserRepo.GetUserByUsername - r.Builder: %v", err)
+		return entity.User{}, fmt.Errorf("UserRepo.GetUserByUsername - r.Builder: %v", err)
+	}
+
+	var user entity.User
+	err = r.Pool.QueryRow(ctx, sql, args...).Scan(
+		&user.Id,
+		&user.Name,
+		&user.Username,
+		&user.Password,
+		&user.Email,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+		&user.Description,
+		&user.ArticlesCount,
+		&user.CommentsCount,
+		&user.FavoritesArticlesCount,
+		&user.FavoritesCommentsCount,
+		&user.FollowersCount,
+		&user.FollowingCount,
+	)
+	if err != nil {
+		log.Errorf("UserRepo.GetUserByUsername - r.Pool.QueryRow: %v", err)
+		if err == pgx.ErrNoRows {
+			return entity.User{}, repoerrs.ErrUserNotFound
+		}
+		return entity.User{}, fmt.Errorf("UserRepo.GetUserByUsername - r.Pool.QueryRow: %v", err)
 	}
 
 	return user, nil
